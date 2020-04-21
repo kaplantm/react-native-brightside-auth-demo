ef261a593acc5dc28dcc89173bc4b8cb73ee8fe3
Toni Kaplan <tonarie.k@gmail.com>
2020-04-21 10:47:13 -0400

    init

---
+++ ios/BrightsideAuth.m
@@ -0,0 +1,15 @@
+//
+//  BrightsideAuth.m
+//  BrightsideAuthDemo
+//
+//  Created by Toni Kaplan on 4/21/20.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(BrightsideAuth, NSObject)
+
+RCT_EXTERN_METHOD(setup);
+
+@end
